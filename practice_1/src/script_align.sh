35c9b5b69088b276fc31e27793978ea7f19ff208
sergiobemar <sergiobenitomartin@gmail.com>
2022-01-09 19:48:27 +0100

    Created bwa functions and fixing some logs

--- practice_1/src/script_align.sh
+++ practice_1/src/script_align.sh
@@ -24,7 +24,7 @@ function_bowtie_alignement_genome() {
     path_results=$4
 
     # Create directory for results
-    results_directory="${path_results}/bowtie_results_mismatch_mismatches_${number_mismatch}"
+    results_directory="${path_results}/bowtie_results_mismatch_${number_mismatch}"
     mkdir $results_directory
 
     for f in $(find $path_data -name "*.fastq")
@@ -47,7 +47,8 @@ function_bowtie_alignement_genome() {
 
         # Alignement using bowtie with 1 mismatch
         echo ""
-        echo "$(function_get_now) function_alignement_genome(): Aligning genome using bowtie"
+        echo "$(function_get_now) function_alignement_genome(): Aligning genome using bowtie."
+        echo "Command: bowtie -t -p 16 -v ${number_mismatch} -S ${index_ref} ${f} ${sam_result}"
         bowtie -t -p 16 -v $number_mismatch -S $index_ref $f $sam_result
 
         # Compress .sam to .bam file
@@ -59,6 +60,63 @@ function_bowtie_alignement_genome() {
         samtools index $bam_result $bai_result
     done
 }
+
+##############################################################################
+# Alignes a genome with its referency gen using BWA 
+# with the number of mismatches setting by an argument.
+#
+# It creates a .sam file with the alignement which will be compressed to a .bam file
+#
+# Arguments:
+#   - Reference gen indexed previously
+#   - Path of directory where the data is stored
+#   - Path of directory where the results is going to be saved
+##############################################################################
+function_bwa_alignement_genome() {
+
+    # Use a more readable variables for input parameters
+    index_ref=$1
+    path_data=$2
+    path_results=$3
+
+    # Create directory for results
+    results_directory="${path_results}/bwa_results"
+    mkdir $results_directory
+
+    for f in $(find $path_data -name "*.fastq")
+    do
+       
+        # Extract file (with extension) and filename (without extension)
+        file=$(basename $f)
+        filename_result="$results_directory/${file%.*}_${2}"
+        dir=$(dirname $f)
+
+        # Create the variables with the names for files SAM, BAM and BAI
+        sam_result="${filename_result}.sam"
+        bam_result="${filename_result}.bam"
+        bai_result="${filename_result}.bai"
+
+        echo ""
+        echo "--------------------------------------------------------------------------------------------------------------------------------------"
+        echo "$(function_get_now) function_bwa_alignement_genome(): Align file ${f} to ${sam_result}"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------"
+
+        # Alignement using bowtie with 1 mismatch
+        echo ""
+        echo "$(function_get_now) function_bwa_alignement_genome(): Aligning genome using BWA."
+        echo "Command: bwa mem ${index_ref} -t 16 ${f} > ${sam_result}"
+        bwa mem $index_ref -t 16 $f > $sam_result
+
+        # Compress .sam to .bam file
+        function_compress_sam $sam_result $bam_result
+
+        # Index .bam result to baiecho 
+	    echo ""
+        echo "$(function_get_now) function_bwa_alignement_genome(): Indexing BAM file, the result will be saved in ${bai_result}"
+        samtools index $bam_result $bai_result
+    done
+}
+
 
 ##############################################################################
 # Compress a .sam file input to a .bam file. Then, it remove the .sam file
@@ -117,7 +175,9 @@ function_bwa_create_index() {
     echo "--------------------------------------------------------------------------------------------------------------------------------------"
     echo "$(function_get_now) function_bwa_create_index(): Start indexing ${input_genome_path} to ${output_index_name}"
     echo "--------------------------------------------------------------------------------------------------------------------------------------"
-     bwa index -p $output_index_name $input_genome_path
+    echo ""
+    echo "$(function_get_now) Launching command: bwa index -p ${output_index_name} ${input_genome_path}"
+    bwa index -p $output_index_name $input_genome_path
 }
 
 ##############################################################################
@@ -154,16 +214,15 @@ main_bowtie_align() {
 #   - Path of directory where the data is going to be stored
 #   - Path of directory where the results is going to be stored
 ##############################################################################
-main_bowtie_align() {
+main_bwa_align() {
 
     # Use a more readable variables for input parameters
     path_bowtie_index=$1
-    number_mismatch=$2
-    path_data=$3
-    path_results=$4
+    path_data=$2
+    path_results=$3
 
     # Align gen samples with reference genome
     echo ""
-    echo "$(function_get_now) Launching command: function_bowtie_alignement_genome ${path_bowtie_index} ${number_mismatch} ${path_data} ${path_results}"
-    function_bowtie_alignement_genome $path_bowtie_index $number_mismatch $path_data $path_results
+    echo "$(function_get_now) Launching command: main_bwa_align ${path_bowtie_index} ${path_data} ${path_results}"
+    function_bwa_alignement_genome $path_bowtie_index $path_data $path_results
 }
