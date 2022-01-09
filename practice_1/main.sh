e5860a508a95d8b89920cda61b8c348d0372ec7a
sergiobemar <sergiobenitomartin@gmail.com>
2022-01-09 15:18:20 +0100

    Updated

--- practice_1/main.sh
+++ practice_1/main.sh
@@ -1,13 +1,15 @@
 # Constants declaration
 SRC="./src"
+SCRIPT_COLLECT_DATA="${SRC}/script_collect_data.sh"
 SCRIPT_BOWTIE_ALIGN="${SRC}/script_align_bowtie.sh"
 
+# Arguments for alignment
+REFERENCE_GENOME="s_cerevisiae.fasta"
+
 # Load scripts
+#source $SCRIPT_COLLECT_DATA
 source $SCRIPT_BOWTIE_ALIGN
 
-# Constants declaration
-REFERENCE_GENOME="s_cerevisiae.fasta"
-NUMBER_MISSMATCHES=1
-
 # Launch script allowing 1 missmatch
+NUMBER_MISSMATCHES=1
 main_bowtie_align $REFERENCE_GENOME $NUMBER_MISSMATCHES
