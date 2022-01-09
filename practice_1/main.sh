503f605fcda19e54f53bc61cd8fbb826a24ddf5d
Sergio <sergiobenitomartin@gmail.com>
2022-01-09 17:29:13 +0100

    Fix parameters

--- practice_1/main.sh
+++ practice_1/main.sh
@@ -27,4 +27,4 @@ function_bowtie_create_index $REFERENCE_GENOME $INDEX_FILES
 
 # Launch script allowing 1 missmatch
 NUMBER_MISSMATCHES=1
-main_bowtie_align $PATH_BOWTIE_INDEX $NUMBER_MISSMATCHES $PATH_DATA $PATH_RESULTS
+main_bowtie_align $INDEX_FILES $NUMBER_MISSMATCHES $PATH_DATA $PATH_RESULTS
